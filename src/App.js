26cfdf6903e628ecad6f68c34c73989324865190
mycode-Quoc-Do <QuocPhu.Do@seattlecolleges.edu>
2025-12-29 01:34:42 -0800

    create pages and routes

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import Home from "./pages/Home.jsx";
+import Profile from "./pages/Profile.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import ForgotPassword from "./pages/ForgotPassword.jsx";
+import Offers from "./pages/Offers.jsx";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
