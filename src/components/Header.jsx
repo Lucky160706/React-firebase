33c8d0d7db7e02df4fbde8021d223fe1fb1e615f
mycode-Quoc-Do <QuocPhu.Do@seattlecolleges.edu>
2025-12-29 16:04:55 -0800

    create sign in, sign up and forgot password pages and oauth component

---
+++ src/components/Header.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+export default function Header() {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    function pathMatchRoute(route) {
+        return route === location.pathname;
+    }
+
+    return (
+        <div className="shadow-sm bg-white border-b sticky top-0 z-50">
+            <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
+                <div>
+                    <img
+                        src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
+                        alt="logo"
+                        className="h-5 cursor-pointer"
+                        onClick={() => navigate("/")}
+                    />
+                </div>
+                <div>
+                    <ul className="flex space-x-10">
+                        <li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px]
+                                ${pathMatchRoute("/")
+                                ? "text-black border-b-red-500"  // Class khi Active
+                                : "text-gray-400 border-b-transparent" // Class khi Inactive
+                            }`}
+                            onClick={() => navigate("/")}>Home</li>
+                        <li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px]
+                                ${pathMatchRoute("/offers")
+                                ? "text-black border-b-red-500"  // Class khi Active
+                                : "text-gray-400 border-b-transparent" // Class khi Inactive
+                            }`}
+                            onClick={() => navigate("/offers")}>Offers</li>
+                        <li className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px]
+                                ${pathMatchRoute("/sign-in")
+                                ? "text-black border-b-red-500"  // Class khi Active
+                                : "text-gray-400 border-b-transparent" // Class khi Inactive
+                            }`}
+                            onClick={() => navigate("/sign-in")}>Sign in</li>
+                    </ul>
+                </div>
+            </header>
+        </div>
+    );
+}
